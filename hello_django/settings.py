9c96fdd01c7a9f9f899e820d00cc7221f2fb4b9a
Luciano Martins <zicadopv@gmail.com>
2023-03-04 10:04:17 -0300

    Create requirements.txt

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -1,11 +1,11 @@
 from pathlib import Path
-from decouple import config
+from decouple import config, Csv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
-ALLOWED_HOSTS = config('ALLOWED_HOSTS')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 INSTALLED_APPS = [
     'django.contrib.admin',
